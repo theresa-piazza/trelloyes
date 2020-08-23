0aefb9f3106234b4bfc6abf4e6d7a850337ad534
Theresa Piazza <tpiazza7867@gmail.com>
2020-08-23 14:18:11 -0400

    add/delete functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
-import React, { Component } from 'react';
-import List from './List';
-import './App.css';
+import React, { Component } from 'react'
+import List from './List'
+import STORE from './STORE'
+import './App.css'
+
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
+
+function omit(obj, keyToOmit) {
+  let {[keyToOmit]: _, ...rest} = obj;
+  return rest;
+}
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
+  state = {
+    store : STORE,
+  };
+
+  handleDeleteCard = (cardId) => {
+    const { lists, allCards } = this.state.store;
+    const newLists = lists.map(list => ({
+      ...list,
+      cardIds: list.cardIds.filter(id => id !== cardId)
+    }));
+
+    const newCards = omit(allCards, cardId);
+    
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards,
+      }
+    })
+  };
+
+  handleAddCard = (listId) => {
+    const newCard = newRandomCard()
+
+    const newLists = this.state.store.lists.map(list => {
+      if (list.id === listId) {
+        return {
+          ...list,
+          cardIds: [...list.cardIds, newCard.id]
+        };
+      }
+      return list;
+    })
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: {
+          ...this.state.store.allCards,
+          [newCard.id]: newCard
+        }
+      }
+    })
   };
 
 render() {
-  const { store } = this.props
+  const { store } = this.state
   return (
     <main className='App'>
       <header className='App-header'>
@@ -21,8 +75,11 @@ render() {
         {store.lists.map(list => (
           <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              onClickDelete={this.handleDeleteCard}
+              onClickAdd={this.handleAddCard}
           />
         ))}
       </div>
--- src/List.test.js
+++ src/List.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
+import Card from './Card';
 import renderer from 'react-test-renderer';
 
 describe('List tests', () => {
